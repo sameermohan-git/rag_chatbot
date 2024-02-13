cf024db9c638516dfa10296d0a712da451a4d712
sameermohan-git <mohansameer.seedling@gmail.com>
2024-02-13 00:47:55 -0500

    fix

--- app_rag.py
+++ app_rag.py
@@ -208,8 +208,8 @@ def handle_enter():
                                                         return_source_documents=True,
                                                         verbose=True)
                     result = qa({"question": user_input, "chat_history": st.session_state.chat_history})
-                    sources = [doc.metadata for doc in result["source_documents"]]
-                    filenames = "["+"][".join(set([x.metadata['filepath'] for x in result['source_documents']]))+"]" 
+                    #sources = [doc.metadata for doc in result["source_documents"]]
+                    filenames = "["+"][".join(set([x.metadata['filenames'] for x in result['source_documents']]))+"]" 
                     response = result["answer"]+ '\n sources:' + {str(filenames)}
                     st.session_state.chat_history.append("Bot", response)
                 except Exception as e:
