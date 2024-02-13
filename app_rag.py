5e06ffb10fc3fb4d48a889fb69c5b9d1dd5bab6c
sameermohan-git <mohansameer.seedling@gmail.com>
2024-02-13 00:52:46 -0500

    remove sources

--- app_rag.py
+++ app_rag.py
@@ -208,9 +208,10 @@ def handle_enter():
                                                         return_source_documents=True,
                                                         verbose=True)
                     result = qa({"question": user_input, "chat_history": st.session_state.chat_history})
-                    #sources = [doc.metadata for doc in result["source_documents"]]
+                    sources = [doc.metadata for doc in result["source_documents"]]
                     filenames = "["+"][".join(set([x.metadata['filenames'] for x in result['source_documents']]))+"]" 
-                    response = result["answer"]+ '\n sources:' + {str(filenames)}
+                    print("sources:" ,sources)
+                    response = result["answer"]
                     st.session_state.chat_history.append(("Bot", response))
                 except Exception as e:
                     st.session_state.chat_history.append(("Bot", f"Error - {e}"))
